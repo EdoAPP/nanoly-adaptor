2135f12cc360103cc899c7c31d8d76f6b4c8c791
ztcrypto <trevonswcr@gmail.com>
2022-11-15 07:48:40 -0500

    updated the adapter by new API endpoint

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const { gql } = require("graphql-request");
-const { request } = require("graphql-request");
 const utils = require("./utils");
 const ChainId = {
   ETHEREUM: 1,
@@ -13,49 +11,16 @@ const ChainNameById = {
   [ChainId.OPTIMISM]: "optimism"
 };
 
-const queryDailyPoolStatus = gql`
-  {
-    dailyPoolStatuses(orderBy: from, orderDirection: desc) {
-      volumeUSD
-      avgFeeInBps
-      feeUSD
-      txCount
-    }
-  }
-`;
-
-/**
- * APY calculation will user the pool status for the last N days.
- * (e.g: If value set to 7, it takes the latest seven-day fee yield from the subgraph)
- */
-const FEE_YIELD_LAST_DAYS = 7;
-
-const DAYS_IN_YEAR = 365;
-
 /**  APIs url constants  */
-const CLIPPER_API = "https://api.clipper.exchange";
-const POOL_STATUS_API = `${CLIPPER_API}/rfq/pool`;
-const SUBGRAPH_BASE_API = "https://api.thegraph.com/subgraphs/name";
-const SUBGRAPH_CHAIN_API = {
-  [ChainId.ETHEREUM]: `${SUBGRAPH_BASE_API}/edoapp/clipper-mainnet`,
-  [ChainId.POLYGON]: `${SUBGRAPH_BASE_API}/edoapp/clipper-polygon`,
-  [ChainId.OPTIMISM]: `${SUBGRAPH_BASE_API}/edoapp/clipper-optimism`
-};
+const CLIPPER_POOL_API = "https://clipper.exchange/api/apy";
 /** */
 
 const getData = async (chainId) => {
-  const [poolStatus, dailyPoolStatusesData] = await Promise.all([
-    utils.getData(`${POOL_STATUS_API}?chain_id=${chainId}`),
-    request(SUBGRAPH_CHAIN_API[chainId], queryDailyPoolStatus)
-  ]);
-
-  return {
-    dailyPoolStatuses: dailyPoolStatusesData.dailyPoolStatuses,
-    poolStatus
-  };
+  const poolStatus = await utils.getData(`${CLIPPER_POOL_API}?chain=${chainId}`);
+  return poolStatus;
 };
 
-const buildPoolInfo = (chainName, poolStatus, dailyPoolStatuses) => {
+const buildPoolInfo = (chainName, poolStatus) => {
   const { value_in_usd, address } = poolStatus.pool;
   const assetSymbols = poolStatus.assets.map((asset) => asset.name).join("-");
   const formattedSymbol = utils.formatSymbol(assetSymbols);
@@ -64,38 +29,25 @@ const buildPoolInfo = (chainName, poolStatus, dailyPoolStatuses) => {
     (asset) => (composition[asset.name] = asset.balance)
   );
 
-  const lastDaysPoolStatus = dailyPoolStatuses.slice(0, FEE_YIELD_LAST_DAYS);
-  const lastDayAggStatus = lastDaysPoolStatus.reduce(
-    (prev, dailyPoolStatus) => {
-      return {
-        totalVolume: prev.totalVolume + +dailyPoolStatus.volumeUSD,
-        totalFee: prev.totalFee + +dailyPoolStatus.feeUSD
-      };
-    },
-    { totalVolume: 0, totalFee: 0 }
-  );
-  const annualizedFee =
-    lastDayAggStatus.totalFee * (DAYS_IN_YEAR / FEE_YIELD_LAST_DAYS);
-  const apy = (annualizedFee * 100) / value_in_usd;
   return {
     pool: address,
     chain: utils.formatChain(chainName),
     project: "clipper",
     symbol: formattedSymbol,
     tvl: value_in_usd,
-    apy,
+    apy: poolStatus.adjustedApy,
     reward: 0,
     rewards: {},
-    base: apy,
+    base: poolStatus.adjustedApy,
     composition
   };
 };
 
 const topLvl = async (chainId) => {
-  const { dailyPoolStatuses, poolStatus } = await getData(chainId);
+  const poolStatus = await getData(chainId);
   const chainName = ChainNameById[chainId];
 
-  return buildPoolInfo(chainName, poolStatus, dailyPoolStatuses);
+  return buildPoolInfo(chainName, poolStatus);
 };
 
 const main = async () => {
